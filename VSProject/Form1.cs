0cca51af5159be9fb3a70ac13f5618726099cb5c
christojeffrey <71057204+christojeffrey@users.noreply.github.com>
2022-03-16 11:33:10 +0700

    bfs graph visualization works

--- VSProject/Form1.cs
+++ VSProject/Form1.cs
@@ -16,6 +16,9 @@ namespace VSProject
         static class GlobalVariable
         {
             public static string output = "";
+            public static Microsoft.Msagl.Drawing.Graph outputGraph = new Microsoft.Msagl.Drawing.Graph("graph");
+            public static Microsoft.Msagl.GraphViewerGdi.GViewer viewer = new Microsoft.Msagl.GraphViewerGdi.GViewer();
+            public static int counter = 1;
         }
         [STAThread]
         public static void Main(string[] args)
@@ -24,15 +27,41 @@ namespace VSProject
             System.Console.WriteLine("fuck the World!");
             System.Console.WriteLine("this is the program entry point! im not sure this thing will show up");
             Form1 FolderCrawler = new Form1();
+
+            //create a viewer object 
+            
+            //create a graph object 
+
+            //create the graph content 
+            // GlobalVariable.outputGraph.AddEdge("A", "B");
+            // GlobalVariable.outputGraph.AddEdge("B", "C");
+            //GlobalVariable.outputGraph.AddEdge("A", "C").Attr.Color = Microsoft.Msagl.Drawing.Color.Green;
+            GlobalVariable.outputGraph.AddNode(GlobalVariable.counter.ToString());
+            GlobalVariable.counter++;
+            //GlobalVariable.outputGraph.FindNode("A").Attr.FillColor = Microsoft.Msagl.Drawing.Color.Magenta;
+            //GlobalVariable.outputGraph.FindNode("B").Attr.FillColor = Microsoft.Msagl.Drawing.Color.MistyRose;
+
+            //bind the graph to the viewer 
+            GlobalVariable.viewer.Graph = GlobalVariable.outputGraph;
+            //associate the viewer with the form 
+            FolderCrawler.SuspendLayout();
+            GlobalVariable.viewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            FolderCrawler.Controls.Add(GlobalVariable.viewer);
+
+
             Application.Run(FolderCrawler);
         }
         public static void BFS(string path)
         {
-            Queue<string> myQueue = new Queue<string>();
-            myQueue.Enqueue(path);
+            // data yg disimpen itu <parent id, self id, diretory full name>
+            int idcounter = 0;
+            Queue<Tuple<int,int, string>> myQueue = new Queue<Tuple<int,int, string>>();
+            myQueue.Enqueue(new Tuple<int,int, string>(idcounter,idcounter, path));
+
             while (myQueue.Count != 0)
             {
-                string currentDirectory = myQueue.Dequeue();
+                Tuple<int, int, string> currentNode = myQueue.Dequeue();
+                string currentDirectory = currentNode.Item3;
                 DirectoryInfo d = new DirectoryInfo(currentDirectory);
                 if (Directory.Exists(currentDirectory))
                 {
@@ -40,17 +69,25 @@ namespace VSProject
                     FileInfo[] Files = d.GetFiles(); // getting files
                     foreach (DirectoryInfo directory in Directories)
                     {
-                        myQueue.Enqueue(@directory.FullName);
+                        myQueue.Enqueue(new Tuple<int,int, string>(currentNode.Item2,++idcounter,directory.FullName));
+
                     }
 
                     foreach (FileInfo file in Files)
                     {
-                        myQueue.Enqueue(file.FullName);
+                        myQueue.Enqueue(new Tuple<int,int , string>(currentNode.Item2, ++idcounter, file.FullName));
                     }
                 }
 
                 //this is where you code the 'action'
-                GlobalVariable.output = GlobalVariable.output + d.Name + "\n";
+                //GlobalVariable.output = GlobalVariable.output + d.Name + "\n";
+                GlobalVariable.outputGraph.AddNode(currentNode.Item2.ToString()).Label.Text = currentNode.Item1 + ", " + currentNode.Item2 + ", "+  d.Name;
+                if(currentNode.Item2 != 0)
+                {
+                    GlobalVariable.outputGraph.AddEdge(currentNode.Item1.ToString(), currentNode.Item2.ToString());
+                }
+
+
             }
         }
 
@@ -87,6 +124,10 @@ namespace VSProject
 
         private void btnHelloWorld_Click(object sender, EventArgs e)
         {
+            //GlobalVariable.outputGraph.AddNode(GlobalVariable.counter.ToString()).Label.Text = "testing";
+            //GlobalVariable.outputGraph.AddEdge("1", "2");
+
+
             FolderBrowserDialog folderDlg = new FolderBrowserDialog();
             DialogResult result = folderDlg.ShowDialog();
            
@@ -103,9 +144,13 @@ namespace VSProject
 
         private void btnBFS_Click(object sender, EventArgs e)
         {
+            GlobalVariable.counter = 0;
             GlobalVariable.output = "";
             BFS(@lblDirectory.Text);
             lblOutput.Text = GlobalVariable.output;
+            // outputing to graph
+            GlobalVariable.viewer.Graph = GlobalVariable.outputGraph;
+
         }
 
         private void btnDFS_Click(object sender, EventArgs e)
